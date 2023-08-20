fc9fe9bd0b967d091824355cd32e9130a80c3d91
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-08-20 10:02:41 +0300

    Friends fix

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { FriendItem, FriendStatus, FriendName } from './Friend.styled';
 
 export const Friend = ({ avatar, name, isOnline }) => {
@@ -8,4 +9,10 @@ export const Friend = ({ avatar, name, isOnline }) => {
       <FriendName>{name}</FriendName>
     </FriendItem>
   );
+};
+
+Friend.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -6,8 +6,8 @@ import { List } from './FriendList.styled';
 export const FriendsList = ({ friends }) => {
   return (
     <List>
-      {friends.map(({ avatar, name, isOnline }) => (
-        <Friend avatar={avatar} name={name} isOnline={isOnline} key={name} />
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <Friend avatar={avatar} name={name} isOnline={isOnline} key={id} />
       ))}
     </List>
   );
